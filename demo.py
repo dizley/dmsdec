ac3fd048ce18a24278f6f86b3f89c5b99938cad0
James Disley <dizley@googlemail.com>
2014-11-13 10:11:31 -0700

    Completed dec2dms function, added optional rounding parameter to dms2dec and updated demo.

--- demo.py
+++ demo.py
@@ -8,5 +8,8 @@ import dmsdec
 
 if __name__ == '__main__':
     location = ('50 3 58.76N', '5 42 53.1W')
+    print(location)
     dec_location = dmsdec.dms2dec(location)
     print(dec_location)
+    print(dmsdec.dec2dms(dec_location, ','))
+    print(dmsdec.dms2dec(dmsdec.dec2dms(dec_location)))
--- dmsdec.py
+++ dmsdec.py
@@ -5,8 +5,9 @@ Created on Nov 6, 2014
 '''
 
 import re
+import math
 
-def dms2dec(location):
+def dms2dec(location, ndecimals=6):
     lon, lat = location[0], location[1]
     
     numbers = re.compile(r"(\d+\.?\d*)")
@@ -19,8 +20,8 @@ def dms2dec(location):
     
     #print(lon_direction, lat_direction)
     
-    lon_dec = float(lon_deg) + float(lon_min)/60 + float(lon_sec)/3600
-    lat_dec = float(lat_deg) + float(lat_min)/60 + float(lat_sec)/3600
+    lon_dec = round(float(lon_deg) + float(lon_min)/60 + float(lon_sec)/3600, ndecimals)
+    lat_dec = round(float(lat_deg) + float(lat_min)/60 + float(lat_sec)/3600, ndecimals)
     
     if(lon_direction == 'S'):
         lon_dec = lon_dec*-1
@@ -29,5 +30,32 @@ def dms2dec(location):
     
     return (lon_dec, lat_dec)
 
-def dec2dms():
-    pass
+def dec2dms(location, seperator=' '):
+    lon, lat = location[0], location[1]
+    
+    south = False
+    west = False
+    
+    if(lon < 0): 
+        lon=lon*-1
+        south = True
+    if(lat < 0): 
+        lat=lat*-1
+        west = True
+    
+    lon_deg = math.floor(lon)
+    lon_min = math.floor((lon - lon_deg) * 60)
+    lon_sec = round((lon*3600 - lon_deg*3600 - lon_min*60), 2)
+    
+    lat_deg = math.floor(lat)
+    lat_min= math.floor((lat - lat_deg) * 60)
+    lat_sec = round((lat*3600 - lat_deg*3600 - lat_min*60), 2)
+    
+    lon_str = seperator.join([str(x) for x in [lon_deg, lon_min, lon_sec]])
+    if south: lon_str += 'S'
+    else: lon_str += 'N'
+    lat_str = seperator.join([str(x) for x in [lat_deg, lat_min, lat_sec]])
+    if west: lat_str += 'W'
+    else: lat_str += 'E'
+    
+    return (lon_str, lat_str)
